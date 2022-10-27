b5354dd08d62bf078204145233b22f2065b5f114
quangk22 <113655587+quangk22@users.noreply.github.com>
2022-10-27 08:39:15 +0700

    Add files via upload

---
+++ LoopWhile/src/basic/dev/MainApp.java
@@ -0,0 +1,18 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 999;
+		while (a <= b) {
+			if (a % 3 == 0) {
+				System.out.println(a + " ");
+
+			}
+			a++;
+
+		}
+
+	}
+}
